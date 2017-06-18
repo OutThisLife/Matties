313d37615b926b4e8365d44eb07c465fa1ecf07a
Talasan Nicholson <talasan.nicholson@gmail.com>
2017-06-17 20:06:06 -0500

    setting up matties

--- web/app/themes/matties/front-page.php
+++ web/app/themes/matties/front-page.php
@@ -11,8 +11,7 @@ get_header();
 <!-- CONTENT -->
 <section id="content" role="main" itemprop="MainContentOfPage">
 <div class="row wrapper">
-	<div class="col s12 m4 l6 xl12 z-depth-1">left</div>
-	<div class="col s12 m4 l6 xl12 z-depth-1">right</div>
+	sup
 </div>
 </section>
 
