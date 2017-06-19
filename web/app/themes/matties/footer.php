bae342e8a3a0116691b8bdbb6fdadc9c38aad4e6
Talasan Nicholson <talasan.nicholson@gmail.com>
2017-06-18 22:49:15 -0500

    homepage started

--- web/app/themes/matties/footer.php
+++ web/app/themes/matties/footer.php
@@ -7,6 +7,16 @@
 ?>
 
 <footer id="footer" itemscope itemtype="http://schema.org/WPFooter">
+    <div id="footer-top">
+        <a href="javascript:;">Follow / Newsletter</a>
+        <a href="tel:5124441888">(512) 444-1888</a>
+    </div>
+
+    <div id="footer-bottom">
+        <a class="exit"></a>
+        <p>Reserve a table at our a la carte brunch, every Saturday and Sunday from 11am - 2pm</p>
+        <a href="javascript:;">RESERVE NOW</a>
+    </div>
 </footer>
 
 <?=BackEnd::getOption('extra-scripts')?>
--- web/app/themes/matties/front-page.php
+++ web/app/themes/matties/front-page.php
@@ -10,9 +10,11 @@ get_header();
 
 <!-- CONTENT -->
 <section id="content" role="main" itemprop="MainContentOfPage">
-<div class="row wrapper">
-	sup
-</div>
+
+<slideshow>
+	<figure class="full-bg" style="background-image: url(<?=assetDir?>/img/slide-1.jpg)"></figure>
+</slideshow>
+
 </section>
 
 <?php get_footer() ?>
