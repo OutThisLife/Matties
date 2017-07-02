4922978a2de95eb17e9541dd78066b9fc41c3912
Talasan Nicholson <talasan.nicholson@gmail.com>
2017-07-02 09:08:47 -0500

    fix gallery link

--- web/app/themes/matties/build-menuItem.php
+++ web/app/themes/matties/build-menuItem.php
@@ -1,11 +1,12 @@
 <?php
 $isGallery = get_the_ID() === GALLERY;
 $class = $isGallery ? 'toggle-gallery' : '';
+$url = $isGallery ? 'javascript:;' : get_permalink();
 $img = FrontEnd::getSrc(get_post_thumbnail_id(), 'menu');
 ?>
 
 <figure>
-	<a href="<?php the_permalink() ?>" class="<?=$class?>" style="
+	<a href="<?=$url?>" class="<?=$class?>" style="
 		background-image: url(<?=$img?>);
 	">
 		<img src="<?=$img?>" alt="<?php the_Title() ?>" />
