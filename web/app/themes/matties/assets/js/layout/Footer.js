e0352d409da2d1c1066b3a8e209076ff3301d4dc
Talasan Nicholson <talasan.nicholson@gmail.com>
2017-07-31 17:52:16 -0500

    change social block to hover instead of click

--- web/app/themes/matties/assets/js/layout/Footer.js
+++ web/app/themes/matties/assets/js/layout/Footer.js
@@ -1,13 +1,7 @@
-const $footer = document.getElementById('footer'),
-	  $social = document.querySelector('.social-block > a')
+const $footer = document.getElementById('footer')
 
 if ($footer)
 	$footer.addEventListener('click', e => {
 		if (e.target.classList.contains('exit-footer'))
 			$footer.classList.toggle('close')
-	})
-
-if ($social)
-	$social.addEventListener('click', e => {
-		e.target.offsetParent.classList.add('active')
 	})
